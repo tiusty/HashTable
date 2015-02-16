866c36c820f3388deaa0a291c6ec468a60a325fd
tiusty <awagud@hotmail.com>
2015-02-16 00:36:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,23 +11,31 @@ using namespace std;
 #include <vector>
 #include "LinkList.h"
 #include "HashTable.h"
+#include <fstream>
 
 int main()
 {
-	vector <string> list(7);
+	vector <string> list;
+	// temporary string variable
+	string temp;
 
-	list[0] = "Hello";
-	list[1] = "trust";
-	list[2] = "Mine";
-	list[3] = "what";
-	list[4] = "Possible";
-	list[5] = "major";
-	HashTable firstList;
+	//open myFile which is the file inputed and creates an instream for that file
+	ifstream myFile("wordList.txt");
+
+	// while loop runs as long as not at the end of the file
+	while(!myFile.eof())
+	{
+		// enter a line into the temporary variable and then add it to the list
+		list.push_back((getline(myFile,temp),temp));
+	}
+	//close the file once all the words have been transfered into the data member wds.
+	myFile.close();
+	HashTable firstList(2000000);
 	for(int i=0; i<list.size();i++)
 	{
 		firstList.insertItem(list[i]);
 	}
-	firstList.printHashTable();
+	//firstList.printHashTable();
 	firstList.printHistogram();
 
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
