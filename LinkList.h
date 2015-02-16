9fe6177d9bfc975286fe4e029ec914754bc16c10
tiusty <awagud@hotmail.com>
2015-02-16 00:35:52 -0500

    Update LinkList.h

--- LinkList.h
+++ LinkList.h
@@ -1,36 +1,55 @@
 /*
- * LinkList.h
+ * HashTable.h
  *
  *  Created on: Feb 15, 2015
  *      Author: alex
  */
 
-#ifndef LINKLIST_H_
-#define LINKLIST_H_
+#ifndef HASHTABLE_H_
+#define HASHTABLE_H_
+#include "LinkList.h"
 
-#include <iostream>
-#include <string>
-using namespace std;
-
-//creates a structure of sName
-struct sName
-{
-	string key;
-	sName *pNext;
-};
-
-class LinkList {
-private:
-	sName *head;
-	int length;
+class HashTable {
 public:
-	LinkList();
-	void insertItem (string newWord);
-	bool removeItem (string itemKey);
-	sName * getItem (string itemKey);
-	void printList();
+	//construct an empty Hash table object
+	//Sets the default array size to 15
+	HashTable(int tableLength=15);
+
+	//Add an item to the hash table
+	void insertItem(string word);
+
+	//Delete an Item from the Hash Table
+	// Returns true if the operation is successful
+	bool deleteItem(string itemKey);
+
+	// Returns an item from the Hash Table by key.
+	// If item not found, Null Pointer returned
+	sName * getItemByKey(string itemKey);
+
+	// Display the contents of the Hash Table to console window
+	void printHashTable();
+
+	// Prints a histogram showing Item distribution
+	void printHistogram();
+
+	// Return the number of Items in a location
 	int getLength();
-	virtual ~LinkList();
+
+	// Returns the number of Items in the HashTable
+	int getNumberOfItems();
+
+	// De-allocates all memory used for the Hash Table
+	virtual ~HashTable();
+private:
+	//reference to an array of type LinkList
+	LinkList * array;
+
+	//length of the hash table
+	int length;
+
+	//returns an array location for a given item key
+	int hash( string itemKey);
+
 };
 
-#endif /* LINKLIST_H_ */
+#endif /* HASHTABLE_H_ */
